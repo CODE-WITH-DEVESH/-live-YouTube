aff6bced0337e89e9d006d6f3c86fa462d677cb8
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-06-30 23:36:25 +0530

    udpate

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -28,3 +28,33 @@ const sum = num1 + num2;
 
 // Print the result
 console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+// studentData.js
+
+// Define a function to display student details
+function displayStudentData(student) {
+  console.log("----- Student Details -----");
+  console.log(`Name       : ${student.name}`);
+  console.log(`Roll No.   : ${student.rollNo}`);
+  console.log(`Course     : ${student.course}`);
+  console.log(`Marks      : ${student.marks}`);
+  console.log("----------------------------\n");
+}
+
+// Create multiple student objects
+const student1 = {
+  name: "Devesh Kumar",
+  rollNo: "101",
+  course: "MCA",
+  marks: 89
+};
+
+const student2 = {
+  name: "Anjali Singh",
+  rollNo: "102",
+  course: "MCA",
+  marks: 92
+};
+
+// Call the function with each student object
+displayStudentData(student1);
+displayStudentData(student2);
